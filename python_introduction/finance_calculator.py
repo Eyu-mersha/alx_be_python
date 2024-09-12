76f97921b451c68a4095b6c099bf2ab910b060fb
Eyu-mersha <eyuamersha@gmail.com>
2024-09-12 22:28:55 +0300

    bones challange done

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -3,7 +3,6 @@ total_monthly_expense = input("Enter your total monthly expenses: ")
 
 monthly_savings = monthly_income - total_monthly_expense
 
-Assume a simple annual interest rate of 5%.
 projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 
 print("Your monthly savings are ", monthly_savings)
