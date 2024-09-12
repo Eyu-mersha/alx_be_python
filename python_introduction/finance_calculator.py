95bca045eee899b780af3550e94c469089380c4f
Eyu-mersha <eyuamersha@gmail.com>
2024-09-12 22:45:01 +0300

    bonus challange updated

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -5,5 +5,5 @@ monthly_savings = monthly_income - monthly_expense
 
 projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 
-print("Your monthly savings are ", monthly_savings ,".")
-print("Projected savings after one year, with interest, is: ", projected_savings ,"." )
+print("Your monthly savings are $", monthly_savings ,".")
+print("Projected savings after one year, with interest, is: $", projected_savings ,"." )
