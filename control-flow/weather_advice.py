49b128c258be85f033585d72f69fdd51b755de7c
Eyu-mersha <eyuamersha@gmail.com>
2024-09-20 23:08:25 +0300

    added daily reminder project

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -5,7 +5,7 @@ weather = input("What's the weather like today? (sunny/rainy/cold): ").lower()
 
 # Provide Clothing Recommendations
 if weather == "sunny" :
-    recommendation = " Wear a t-shirt and sunglasses."
+    recommendation = "Wear a t-shirt and sunglasses."
 elif weather == "rainy" :
     recommendation = "Don't forget your umbrella and a raincoat."
 elif weather == "cold" :
