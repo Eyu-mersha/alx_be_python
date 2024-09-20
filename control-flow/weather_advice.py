d5d7f59534628caa16a719325fb89a7afebd6c32
Eyu-mersha <eyuamersha@gmail.com>
2024-09-20 23:12:40 +0300

    added daily reminder project

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,14 +1,17 @@
 # weather_advice.py
 
-# Prompt user for weather input
+# Prompt User for Weather Input
 weather = input("What's the weather like today? (sunny/rainy/cold): ").lower()
 
-# Provide clothing recommendations based on user input
+# Provide Clothing Recommendations
 if weather == "sunny":
-    print("Wear a t-shirt and sunglasses.")
+    recommendation = "Wear a t-shirt and sunglasses."
 elif weather == "rainy":
-    print("Don't forget your umbrella and a raincoat.")
+    recommendation = "Don't forget your umbrella and a raincoat."
 elif weather == "cold":
-    print("Make sure to wear a warm coat and a scarf.")
-else:
-    print("Sorry, I don't have recommendations for this weather.")
+    recommendation = "Make sure to wear a warm coat and a scarf."
+else : 
+    recommendation = "Sorry, I don't have recommendations for this weather."
+
+# Output the Recommendation
+print(recommendation)
