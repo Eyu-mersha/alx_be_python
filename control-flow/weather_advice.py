8f0c8c2f5fddd17b97618ec913b5a8efec338c3b
Eyu-mersha <eyuamersha@gmail.com>
2024-09-20 21:57:48 +0300

    control flow project added

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,12 @@
+answer = input("What's the weather like today? (sunny/rainy/cold):")
+recommend = ""
+if answer == "sunny":
+  recommend = "wear a t-shirt and sunglasses."
+elif answer == "rainy":
+  recommend = "Don't forget your umbrella and a raincoat."
+elif answer == "cold":
+  recommend = "Make sure to wear a warm coat and a scarf."
+else:
+  recommend = "Sorry, I don't have recommendations for this weather."
+
+print(recommend)
