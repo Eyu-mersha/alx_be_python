fb35ec48d6ac4a8e382014f554f000a8bbb70135
Eyu-mersha <eyuamersha@gmail.com>
2024-09-20 22:31:55 +0300

    added pattern drawing

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,12 +1,10 @@
 size = int(input("Enter the size of the pattern:"))
 
 
-# Initialize row counter
 row = 0
 
-# Draw the Pattern
 while row < size:
     for _ in range(size):
         print("*", end="")
-    print()  # Move to the next line after finishing a row
+    print() 
     row += 1
