a52d5e8319d685da0f36fbcae83773074c5a2bc2
Eyu-mersha <eyuamersha@gmail.com>
2024-09-27 19:36:44 +0300

    added a new file for the second function project

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,38 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            item= input("What is the item you want to add")
+            shopping_list.append(item)
+            pass
+        elif choice == '2':
+            # Prompt for and remove an item
+            item= input("What is the item you want to remove")
+            if shopping_list.__contains__(item):
+                shopping_list.remove(item)
+            else:
+                print("Item not found in the list")    
+            pass
+        elif choice == '3':
+            # Display the shopping list
+            for i in shopping_list:
+                print(i)
+            pass
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
