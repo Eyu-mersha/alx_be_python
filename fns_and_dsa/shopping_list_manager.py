5309110382590268bb857f26fb3f0f7ce97e4a50
Eyu-mersha <eyuamersha@gmail.com>
2024-09-27 19:46:39 +0300

    updated a new file for the second function project

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,3 +1,5 @@
+# shopping_list_manager.py
+
 def display_menu():
     print("Shopping List Manager")
     print("1. Add Item")
@@ -7,30 +9,39 @@ def display_menu():
 
 def main():
     shopping_list = []
+    
     while True:
         display_menu()
         choice = input("Enter your choice: ")
 
         if choice == '1':
-            item= input("What is the item you want to add")
+            # Prompt for and add an item
+            item = input("Enter the item to add: ")
             shopping_list.append(item)
-            pass
+            print(f"{item} has been added to the shopping list.")
+
         elif choice == '2':
             # Prompt for and remove an item
-            item= input("What is the item you want to remove")
-            if shopping_list.__contains__(item):
+            item = input("Enter the item to remove: ")
+            if item in shopping_list:
                 shopping_list.remove(item)
+                print(f"{item} has been removed from the shopping list.")
             else:
-                print("Item not found in the list")    
-            pass
+                print(f"{item} not found in the shopping list.")
+
         elif choice == '3':
             # Display the shopping list
-            for i in shopping_list:
-                print(i)
-            pass
+            if shopping_list:
+                print("Current shopping list:")
+                for idx, item in enumerate(shopping_list, start=1):
+                    print(f"{idx}. {item}")
+            else:
+                print("Your shopping list is empty.")
+
         elif choice == '4':
             print("Goodbye!")
             break
+        
         else:
             print("Invalid choice. Please try again.")
 
