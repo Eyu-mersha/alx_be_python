5af6d2ad0f6b58376083972b9efc1a63813a6e84
Eyu-mersha <eyuamersha@gmail.com>
2024-10-11 18:09:50 +0300

    new commit

--- oop/book_class.py
+++ oop/book_class.py
@@ -5,10 +5,10 @@ class Book:
         self.year = year
 
     def __del__(self):
-        print ("Deleting {self.title}")
+        print(f"Deleting '{self.title}'")
     
     def __str__(self):
-        print("{self.title} by {self.author}, published in {self.year}")
+        return f"'{self.title}' by {self.author}, published in {self.year}"
     def __repr__(self):
-        print(f"{name} is {age} years old.")
+        return f"Book('{self.title}', '{self.author}', {self.year})"
    
