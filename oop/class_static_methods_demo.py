bc983ca99b69caffe92af3b4cd30b7b602bbba73
Eyu-mersha <eyuamersha@gmail.com>
2024-10-11 19:15:50 +0300

    new commit

--- oop/class_static_methods_demo.py
+++ oop/class_static_methods_demo.py
@@ -1,10 +1,12 @@
 class Calculator:
+    calculation_type = "Arithmetic Operations"
     def __init__(self, a, b):
         self.a = a
         self.b = b
 
     @classmethod
     def multiply(cls, a, b):
+        print(f"Calculation type: {cls.calculation_type}")
         return cls(a*b)
 
     @staticmethod
