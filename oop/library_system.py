5b9d25fea4efbafa312248647c9b62fba836f33f
Eyu-mersha <eyuamersha@gmail.com>
2024-10-11 18:40:00 +0300

    new commit

--- oop/library_system.py
+++ oop/library_system.py
@@ -12,4 +12,16 @@ class PrintBook(Book):
     def __init__(self, title, author,page_count):
         super().__init__(title, author)
         self.page_count = page_count
-    
+class Library:
+    def __init__(self):
+        """Initialize a Library instance with an empty list of books."""
+        self.books = []
+
+    def add_book(self, book):
+        """Add a Book, EBook, or PrintBook instance to the library."""
+        self.books.append(book)
+
+    def list_books(self):
+        """Print details of each book in the library."""
+        for book in self.books:
+            print(book)
