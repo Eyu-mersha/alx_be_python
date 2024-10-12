305be1bebb096e59d5ac3db8788ea1ee4856011a
Eyu-mersha <eyuamersha@gmail.com>
2024-10-12 23:24:23 +0300

    sign up and login functionality final update

--- login/login_server.py
+++ login/login_server.py
@@ -9,36 +9,35 @@ class Credentials:
     def __repr__(self):
         return print(f"name:{self.name} Username: {self.username} Password: {self.password}")
     def createUser(name,username,password):
-        credential = Credentials(name,username,hash_password(password))
+        credential = Credentials(name,username,Operations.hash_password(password))
         users.append(credential)
         print("\n-------- Sign up successful.---------")
     def checkUser(username,password):
         for i in users:
-            if username == i.username and hash_password(password)== i.password:
+            if username == i.username and Operations.hash_password(password)== i.password:
                 print(f"\n\n Welcome to your Dashboard dear {i.name} \n\n")
                 
             else: 
                 print("------Wrong username or password")
-                
-    
-     
-def hash_password(password):
-    sha256 = hashlib.sha256()
-    sha256.update(password.encode('utf-8'))
-    return sha256.hexdigest()
-def login():
-    print("\n\n\n------------------------- Login -----------------------------\n\n")
-    print("Submit the required information please")
-    username = input("Enter your phone number:  ")
-    password = getpass.getpass("Enter your password: ")
-    Credentials.checkUser(username,password)
-def signUp():
-    print("\n\n\n-------------------------Sign up form  -----------------------------\n\n")
-    print("Submit the required information please")
-    name = input("Enter your name:  ")
-    username = input("Enter your phone number:  ")
-    password = getpass.getpass("Enter your password: ")
-    Credentials.createUser(name,username,password)
+
+class Operations:                 
+    def hash_password(password):
+        sha256 = hashlib.sha256()
+        sha256.update(password.encode('utf-8'))
+        return sha256.hexdigest()
+    def login():
+        print("\n\n\n------------------------- Login -----------------------------\n\n")
+        print("Submit the required information please")
+        username = input("Enter your phone number:  ")
+        password = getpass.getpass("Enter your password: ")
+        Credentials.checkUser(username,password)
+    def signUp():
+        print("\n\n\n-------------------------Sign up form  -----------------------------\n\n")
+        print("Submit the required information please")
+        name = input("Enter your name:  ")
+        username = input("Enter your phone number:  ")
+        password = getpass.getpass("Enter your password: ")
+        Credentials.createUser(name,username,password)
 
 
 print("\n\n\n-------------------------Welcome to Gulit Online Store-----------------------------\n\n")
@@ -47,10 +46,10 @@ print("       2. Login to your dashboard  \n\n")
 choice = int(input(" Enter your choice: "))
 
 if choice == 1:
-    signUp()
-    login()
+    Operations.signUp()
+    Operations.login()
 elif choice == 2:
-    login()
+    Operations.login()
 else: 
     print("You have entered a wrong number.")
 
